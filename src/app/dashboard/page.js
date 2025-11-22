3fe70fd44d47767a1ae4062c75727053b7dd7e55
abbasyasin1n2 <abbasyasin5n2@gmail.com>
2025-11-23 00:37:08 +0600

    created middleware.js and tested if protected routes are protected

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Secret Dashboard</h1>
+      <p>You are logged in!</p>
+    </div>
+  );
+}
+
---
+++ src/middleware.js
@@ -0,0 +1,14 @@
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  // Matches the pages config in `[...nextauth]`
+  pages: {
+    signIn: '/api/auth/signin',
+  },
+})
+
+export const config = { 
+  // Protects /dashboard and everything inside it
+  matcher: ["/dashboard/:path*"] 
+}
+
